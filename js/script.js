1451bc089fc709bf4a4c23ac6c50587ea641d30c
Victorsousa98 <victorsousa708@gmail.com>
2021-07-26 13:43:52 -0300

    Estilização

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
-
+let score = 0;
 function criarBG(){
     context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -45,6 +45,12 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (i =  1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over! Seu recorde foi de " + score + " frutas");
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -59,18 +65,25 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
      
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
-    snake.pop();
+    
 
     let newHead = {
         x: snakeX,
         y: snakeY
     }   
 
-    snake.unshift(newHead)
+    snake.unshift(newHead);
 }
 
-letjogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 100);
 
 
 
