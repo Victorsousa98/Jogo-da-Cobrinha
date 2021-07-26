bcca80d2d2dc419f37d8298306214acdda2e2ee0
Victorsousa98 <victorsousa708@gmail.com>
2021-07-26 11:10:46 -0300

    Cria movimentos e nova Head

--- js/script.js
+++ js/script.js
@@ -1,11 +1,51 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
-function criarBG(){
-    context.fillstyle = "lightgreen"
-    context.fillReact(0, 0, 16 * box, 16 * box)
-
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
 
-criarBG();
+let direction = "right";
+
+function criarBG(){
+    context.fillStyle = "lightblue";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+    for (i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeX -= box;
+    if(direction == "down") snakeX += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+letjogo = setInterval(iniciarJogo, 100);
+
+
+
+
+
